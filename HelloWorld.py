4efcfd0c2c56b99bedbfe5bf9b08ab7a1a456416
Kapil Dev <kapil.dev87@gmail.com>
2019-08-23 09:17:24 -0700

    Create HelloWorld.py
    This function displays the algorithms and paradigm

---
+++ HelloWorld.py
@@ -0,0 +1,4 @@
+def algorithms():
+    """This function displays the algorithms and paradigm"""
+    algorith_paradigms = ['Divide-and-conquer', 'Backtrackig', 'Greedy-Algorithms', 'Dynamic-programming']
+    return algorith_paradigms
